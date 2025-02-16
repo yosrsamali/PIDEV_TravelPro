e373c8168f744ad22320ec4318e6915f4e9d48fa
Kacem-Trabelsi <25k01a2003c@gmail.com>
2025-02-16 20:39:27 +0100

    Initial commit

---
+++ src/main/java/utils/MyDatabase.java
@@ -0,0 +1,34 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+    public class MyDatabase {
+        private static MyDatabase instance;
+        private final String URL ="jdbc:mysql://127.0.0.1:3306/TravelPro";
+        private final String USERNAME ="root";
+        private final String PASSWORD = "";
+        private Connection  cnx ;
+
+        private MyDatabase() {
+            try {
+                cnx = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+                System.out.println("connected ...");
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+
+        public static MyDatabase getInstance() {
+            if (instance == null)
+                instance = new MyDatabase();
+            return instance;
+        }
+
+        public Connection getCnx() {
+            return cnx;
+        }
+    }
+
