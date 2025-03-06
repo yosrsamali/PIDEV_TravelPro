4144241b4364cd7c6a5e2e3786042a4b6f7fbe7e
ismail-belhoula <belhoulaismail@fsm.u-monastir.tn>
2025-03-06 22:54:46 +0100

    fix the interface

--- src/main/java/tn/esprit/test/MainFX.java
+++ src/main/java/tn/esprit/test/MainFX.java
@@ -17,7 +17,7 @@ public class MainFX extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        FXMLLoader loader =new FXMLLoader(getClass().getResource("/Client_Manages_Panier_Interface.fxml"));
+        FXMLLoader loader =new FXMLLoader(getClass().getResource("/Main_Interface.fxml"));
         try {
             Parent root = loader.load();
             Scene scene = new Scene(root);
